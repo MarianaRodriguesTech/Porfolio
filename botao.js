8d5cf56d1ba93ed6d48cb39ceb20bbd0b955c934
Mariana <141480630+MarianaRodriguesTech@users.noreply.github.com>
2024-03-28 09:45:16 -0300

    Add files via upload

---
+++ botao.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 100) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    });
+});
