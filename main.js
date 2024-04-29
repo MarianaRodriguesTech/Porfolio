a8ff03076109ad4f3b75245d82119e780220531c
Mariana <141480630+MarianaRodriguesTech@users.noreply.github.com>
2024-04-29 12:47:54 +0000

    Atualização

--- main.js
+++ main.js
@@ -10,10 +10,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-const toggleBtn = document.getElementById('toggle');
-const body = document.body;
-
-toggleBtn.addEventListener('click', function(){
-    body.classList.toggle('dark-mode')
-    //body.classlist é para criar uma classe no corpo
-} )
